efaae0902c01026a0080f7fc11b0a58bebf65f08
MD Iftekhar Hossain Tanveer <tanvirhossain1010@gmail.com>
2023-09-17 10:43:54 +0300

    mathmatical operator learnt

--- Base Lessons/testing.js
+++ Base Lessons/testing.js
@@ -1,7 +1,25 @@
 // alert ("This is a demo of alert tesing");
+
+//variable lessons
 var numVariable = 30;
 var stringVariable = "Demo string variable";
 var booleanVariable = true;
 console.log(numVariable);
 console.log(stringVariable);
 console.log(booleanVariable);
+
+
+//mathmatical operator lessons
+var OP1 = 5;
+var OP2 = 15;
+var OP3 = 25;
+var OP4 = 35;
+
+var a = OP1 + OP2;
+console.log(a);
+var b = OP3 + OP2;
+console.log(b);
+var c = OP4 / OP1;
+console.log(c);
+var d = OP4 * OP1 * OP2 * OP3;
+console.log(d);
