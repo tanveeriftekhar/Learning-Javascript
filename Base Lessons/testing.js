62fe92784b2100e463ed346f169779150158c648
MD Iftekhar Hossain Tanveer <tanvirhossain1010@gmail.com>
2023-09-17 14:57:45 +0300

    Playing with if-else in javascript

--- Base Lessons/testing.js
+++ Base Lessons/testing.js
@@ -8,7 +8,6 @@ console.log(numVariable);
 console.log(stringVariable);
 console.log(booleanVariable);
 
-
 //mathmatical operator lessons
 var OP1 = 5;
 var OP2 = 15;
@@ -24,23 +23,23 @@ console.log(c);
 var d = OP4 * OP1 * OP2 * OP3;
 console.log(d);
 
-//shorthand operators 
+//shorthand operators
 var myVar = 10;
-myVar = myVar+5;
+myVar = myVar + 5;
 console.log(myVar);
-myVar += 5
+myVar += 5;
 console.log(myVar);
-myVar -= 10
+myVar -= 10;
 console.log(myVar);
-myVar /= 2
+myVar /= 2;
 console.log(myVar);
 myVar *= 5;
 console.log(myVar);
-myVar++
+myVar++;
 console.log(myVar);
-myVar +=1;
+myVar += 1;
 console.log(myVar);
-myVar++
+myVar++;
 console.log(myVar);
 ++myVar;
 console.log(myVar);
@@ -50,16 +49,36 @@ var myVar = "Tanveer";
 // document.write(myVar);
 console.log(myVar);
 
-// Boolean Test 
-var iLikeMeat = true
+// Boolean Test
+var iLikeMeat = true;
 console.log(iLikeMeat);
 
-var iLikeMeat = false
+var iLikeMeat = false;
 console.log(iLikeMeat);
 
-c = 7>5
+c = 7 > 5;
 console.log(c);
 
-c = 7<5
+c = 7 < 5;
 console.log(c);
 
+// If statement
+var youLikeMeat = true;
+
+if (7 > 5) {
+  document.write("This expression is true");
+}
+
+if (7 < 5) {
+  document.write("This expression is false");
+}
+if (7 == 7) {
+  document.write("This expression is true");
+}
+
+var myNum = 15;
+if (myNum > 10) {
+  document.write("My Num is greater");
+} else {
+  document.write("My num is equal");
+}
