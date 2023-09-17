1b404e6c0fc663abde83b1692e38a3dcf79e2642
MD Iftekhar Hossain Tanveer <tanvirhossain1010@gmail.com>
2023-09-17 10:35:44 +0300

    JS Varibale Done

--- Base Lessons/testing.js
+++ Base Lessons/testing.js
@@ -1,1 +1,7 @@
-alert ("This is a demo of alert tesing");
+// alert ("This is a demo of alert tesing");
+var numVariable = 30;
+var stringVariable = "Demo string variable";
+var booleanVariable = true;
+console.log(numVariable);
+console.log(stringVariable);
+console.log(booleanVariable);
