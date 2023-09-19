897e6fe7d6d4e96b6490a740a3f32ae0400eb596
MD Iftekhar Hossain Tanveer <tanvirhossain1010@gmail.com>
2023-09-20 01:09:11 +0300

    Or operator done

--- Base Lessons/testing.js
+++ Base Lessons/testing.js
@@ -88,50 +88,48 @@ if (myNum > 10) {
 var myAge = 35;
 if (myAge > 30) {
   document.write("You are more than 30");
-}
-else if (myAge > 20) {
+} else if (myAge > 20) {
   document.write("You are more than 20");
-}
-else if (myAge > 10) {
+} else if (myAge > 10) {
   document.write("You are more than 10");
-}
-else {
-  document.write("You are below 10!")
-}
-
-// comparison operator 
-var x = 4; 
-if (x == 5){
-  document.write("The number is correct")
-}
-else {
-  document.write("The number is not correct")
+} else {
+  document.write("You are below 10!");
 }
 
-
-var x = "5"; 
-if (x != 5){
-  document.write("The number is correct")
+// comparison operator
+var x = 4;
+if (x == 5) {
+  document.write("The number is correct");
+} else {
+  document.write("The number is not correct");
 }
-else {
-  document.write("The number is not correct")
+
+var x = "5";
+if (x != 5) {
+  document.write("The number is correct");
+} else {
+  document.write("The number is not correct");
 }
 
-if (x === 5 ){
+if (x === 5) {
   document.write("The number is abs correct");
-}
-else if ( x === "5")
-{
-  document.write("The string is abs correct ")
-}else {
+} else if (x === "5") {
+  document.write("The string is abs correct ");
+} else {
   document.write("Hoyni kichu");
 }
 
-//logical operator 
-var myAge = 25; 
-if ( myAge >= 18 && myAge <=30 ) {
-document.write ("You are an adult");
-}
-else {
+//logical operator
+var myAge = 25;
+if (myAge >= 18 && myAge <= 30) {
+  document.write("You are an adult");
+} else {
   document.write("You are not eligible");
+}
+
+var myAge = 50;
+if (myAge < 10 || myAge > 30) {
+  document.write("You are a school boy");
+} else {
+  document.write("You are not a school boy");
 }
