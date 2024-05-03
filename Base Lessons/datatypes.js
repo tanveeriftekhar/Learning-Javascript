9bdbafe5c9414254bee6635943e3d86b23b387b8
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-05-03 23:25:18 +0300

    learning continues

---
+++ Base Lessons/datatypes.js
@@ -0,0 +1,19 @@
+/*Javascript has two data types 
+Primitive data types
+Non-primitive data types 
+
+Primitive data types are 
+Number 
+String 
+Boolean 
+Undefined 
+Null 
+BigInt 
+Symbol 
+
+
+Non-primitive data types are called Objects 
+Objects are made of "key:value" pairs 
+Objects have some sub-categories which are called arrays, functions etc 
+
+*/
