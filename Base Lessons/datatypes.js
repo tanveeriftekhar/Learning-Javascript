b51bd9fb25c0088a2d6cb930825a54e32ac918bb
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-05-03 23:40:43 +0300

    continues

--- Base Lessons/datatypes.js
+++ Base Lessons/datatypes.js
@@ -17,3 +17,24 @@ Objects are made of "key:value" pairs
 Objects have some sub-categories which are called arrays, functions etc 
 
 */
+
+const student = {
+    fullName : "Iftekhar Hossain Tanveer",
+    age : "27", 
+    cgpa : "3.26",
+    isPass: "true"
+};
+console.log(student);
+
+//we have two ways to get access to a specific key in an object 
+//object["key"]
+//object.key 
+
+console.log(student.fullName);
+console.log(student["fullName"]);
+
+//To change any key we have to do the same 
+
+student["fullName"] = "Tashrin Tarannum Tanisha"
+console.log(student.fullName);
+
